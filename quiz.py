ab0631588107e0c04449153a4fd981cdffaeb3bc
shamshuddin shaik <shamshuddinshaik376@gmail.com>
2026-03-06 17:16:30 +0530

    Clarify print statement comment for loop variable 'i'
    Updated comment to clarify the value of 'i' after the loop.

--- quiz.py
+++ quiz.py
@@ -1,4 +1,4 @@
 # what will it display?
 for i in range(3):
     pass
-print(i)  # It will display 2, as the loop completes and i retains its last value.
+print(i)  # It will display 2, as the loop completes and i value returns its last value(0,1,2).
